9fb69764a7371be388192a4e04bc217708fd97d4
Anderson Lima <anderson.lima-ext@olxbr.com>
2023-01-03 15:04:01 -0300

    chore: configure pre push and pre commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, * as áth from 'path'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({

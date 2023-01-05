e09b855f7a15cc31b7dad99bfea4db4182cfad36
Anderson Lima <anderson.lima-ext@olxbr.com>
2023-01-05 00:08:47 -0300

    test: find text in component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import reactLogo from '@assets/svg/react.svg'
-import './App.css'
-
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    </div>
-  )
+  return <h1>Vite + React</h1>
 }
 
 export default App

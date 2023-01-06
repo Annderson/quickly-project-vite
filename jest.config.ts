bc97b7c9ad430dcefee483e8f0b11acc2f743255
Anderson Lima <anderson.lima-ext@olxbr.com>
2023-01-06 12:04:39 -0300

    chore: add new jest config to alias path

--- jest.config.ts
+++ jest.config.ts
@@ -73,24 +73,27 @@ export default {
 
   modulePaths: ['<rootDir>'],
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: ['node_modules', __dirname],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: [
+    'js',
+    'mjs',
+    'cjs',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node',
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@styles/(.*)': '<rootDir>/src/styles/$1',
+    '@locales/(.*)': '<rootDir>/src/locales/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
